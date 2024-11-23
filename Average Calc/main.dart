feccbbf7d47d0d23c5689f3865663de14477f9d1
maaz <maazulhaq00@gmail.com>
2024-11-23 13:05:45 +0500

    Dart

---
+++ Average Calc/main.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  Map<String, int> studentsScore = {
+    "A": 76,
+    "B": 89,
+    "C": 98,
+    "D": 56
+  };
+
+  double average = CalculateAverage(studentsScore);
+
+  print("Average is $average");
+
+}
+double CalculateAverage(Map<String, int> scoreMap){
+
+  int totalStudents = scoreMap.length;
+  int totalScores = 0;
+  for (var score in scoreMap.values) {
+    totalScores += score; // totalScores = totalScores + score
+  }
+
+  double avg = totalScores / totalStudents;
+  return avg;
+}
